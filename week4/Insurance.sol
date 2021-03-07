198e9ca395f469e71fef81ed83d8de568553c089
nihal1m <62612108+nihal1m@users.noreply.github.com>
2021-03-07 13:06:54 +0300

    Update Insurance.sol

--- week4/Insurance.sol
+++ week4/Insurance.sol
@@ -1,11 +1,23 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.1;
 
+/**
+ * @title Tech Insurance tor
+ * @dev 
+ * Step1: Complete the functions in the insurance smart contract
+ * Step2:Add any required methods that are needed to check if the function are called correctly, 
+ * and also add a modifier function that allows only the owner can run the changePrice function.
+ * Step3: Add any error handling that may occur in any function
+ * Step4: Add a modifer function to check the time if the client insurance is valid.
+ * Step5 (opcional): Add a refund function that refunds money back to the client after one week. Guaranteed Money Back Plan.  
+ * Step6: implement ERC 721 Token to this contract and change what it needs to be changed. 
+ * 
+ */
  
-import "../github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-contract TechInsurance is ERC721 {
-
-  
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+contract TechInsurance is ERC721("test","TST"){
+    
     /** 
      * Defined two structs
      * 
@@ -21,64 +33,67 @@ contract TechInsurance is ERC721 {
     struct Client {
         bool isValid;
         uint time;
-
     }
+    
+    
+
+    
+     modifier OnlyinsOwner(uint _prodID) {
+         require(msg.sender == ownerOf(_prodID));
+         _;
+    }
+         modifier TimeCheck {
+         require(block.timestamp <= timeLocked + 15 minutes );
+         _;
+     }
     
     mapping(uint => Product) public productIndex;
     mapping(address => mapping(uint => Client)) public client;
-    
+    uint256 timeLocked = block.timestamp;
     uint productCounter;
     
-    address payable insOwner;
-    constructor(address payable _insOwner) public ERC721("Elite","code"){
-      insOwner = _insOwner;
-   }
+     address payable insOwner;
+    // constructor(address payable _insOwner) public{
+    //     insOwner = _insOwner;
+    // }
  
-    function addProduct(uint _productId, string memory _productName, uint _price ) public {
-        productCounter++;
-        Product memory newProduct =Product(_productId, _productName, _price, true);
-        productIndex[productCounter++] = newProduct;
+    function addProduct(uint _productId, string memory _productName, uint _price ) public  {
         
+       productCounter++;
+       address  prodOwner = msg.sender;
+       productIndex[productCounter]= Product(_productId, _productName, _price, true);
+       _mint(prodOwner, productCounter);
     }
     
-    function doNotOffer(uint _productIndex) public view {
-        require(msg.sender == insOwner, "I'm not offer it");
-        productIndex[_productIndex].offered == false;
-        
-    }
     
-    function forOffer(uint _productIndex) public view   {
-        require(msg.sender == insOwner, "I'm offer it");
-        productIndex[_productIndex].offered ==true;
+    function doNotOffer(uint _productIndex) public  {
 
+        productIndex[_productIndex].offered = false;
     }
+    
+    function forOffer(uint _productIndex) public OnlyinsOwner(_productIndex) {
+        
+        productIndex[_productIndex].offered = true;
+    }
+    
+    function changePrice(uint _productIndex, uint _price) public OnlyinsOwner(_productIndex) {
 
-    function changePrice(uint _productIndex, uint _price) public view  {
-        require(productIndex[_productIndex].price >= 1, "not valid index" );
-        productIndex[_productIndex].price== _price;
-    }
-    
-    // handling the error
-    function setPrice (uint _price) public view  {
-        uint price = _price;
-        require(insOwner == msg.sender, "you are not the owner");
+        productIndex[_productIndex].price = _price;
         
     }
     
-    function buyInsurance(uint _productIndex) public payable {
-        require(productIndex[_productIndex].price == msg.value, "Not appropriate" );
-        require(productIndex[_productIndex].price == 0, "Not valid index");
-        
-        Client memory newClient;
-        newClient.isValid = true;
-        newClient.time = block.number;
-        client[msg.sender][_productIndex] = newClient;
-        insOwner.transfer(msg.value); 
-        
-    }
+    /**
+    * @dev 
+    * Every client buys an insurance, 
+    * you need to map the client's address to the id of product to struct client, using (client map)
+    */
+    
+    function buyInsurance(uint _productIndex) public payable  {
+        require(productIndex[_productIndex].offered == true, "This item is sold out!");
+        require(msg.value <= productIndex[_productIndex].price, "You don't have enough tokens!");      
+        Client(true, block.timestamp);
+        _transfer(ownerOf(_productIndex), msg.sender,_productIndex);   
+        doNotOffer(_productIndex);    
+    } 
+    
 }
-     
-    //function refund(uint productId) public returns(bool success) {
-        
-     
-    
